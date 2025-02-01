47292b7591990caf92f787f0bc99dddc46a08c49
Ashish Tomar <tashish410@gmail.com>
2025-02-01 22:05:47 +0530

    initialized firebase && configured redux store and created authSlice

---
+++ src/store/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+    },
+    reducers: {
+        loginUser(state, action) {
+        state.user = action.payload;
+        },
+        logOutUser(state) {
+        state.user = null;
+        },
+    },
+})
+
+export const { loginUser, logOutUser } = authSlice.actions;
+
+export default authSlice.reducer;
